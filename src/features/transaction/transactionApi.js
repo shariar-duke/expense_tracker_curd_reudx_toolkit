e91853681f2cc500781081afa2bd6d7ccba48213
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-03 16:26:25 +0600

    api post function called

--- src/features/transaction/transactionApi.js
+++ src/features/transaction/transactionApi.js
@@ -2,8 +2,16 @@ import axios from "../../utils/axios"; // importing the axios instance
 
 // First we need to write a function for get 
 
-const getTransactions =async()=> 
+export const getTransactions =async()=> 
 {
   const response = await axios.get("/transactions")
   return response.data
+}
+
+// This function is for creating a new Transiction
+
+export const addTransaction = async(data)=> 
+{
+    const response = await axios.post("/transactions",data)
+    return response.data
 }
