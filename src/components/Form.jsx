7ad3b81cd3ce178e111532831d8a6ae723c2b48e
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-04 11:03:14 +0600

    form submitter successfully

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,45 @@
 import { useState } from "react";
-
+import { useDispatch, useSelector } from "react-redux";
+import { createTransaction } from "../features/transaction/transactionSlice";
 export default function Form() {
-    const[data, setData] = useState({
-        name:"",
-        amount:"",
-        type:""
+  const [data, setData] = useState({
+    name: "",
+    amount: "",
+    type: "",
+  });
+  const dispatch = useDispatch();
+  const { name, amount, type } = data;
+  // we need to check if the form submission is fullfiled , pending or error occured
+  const { isLoading, isError } = useSelector(
+    (state) => state.transaction
+  );
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: name === "amount"? Number(value) : value
+    });
+  };
 
-    })
+  const handleCreate = (e) => {
+    e.preventDefault();
+    dispatch(createTransaction(data));
+    if(!isError) 
+    {
+         // Reset form fields only if there is no error
+      setData({
+        name: "",
+        amount: "",
+        type: "",
+      })
+    }
+  };
 
-    const{name, amount , type} = data
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setData({
-          ...data,
-          [name]: value,  // Dynamically update the field that changed
-        });
-      };
-
-    console.log("The data is", data)
   return (
-    <form className="px-6 py-4 bg-white rounded-lg  space-y-4">
+    <form
+      onSubmit={handleCreate}
+      className="px-6 py-4 bg-white rounded-lg  space-y-4"
+    >
       <p className="text-lg font-semibold">Add New Transaction</p>
 
       {/* Name Field */}
@@ -31,6 +51,8 @@ export default function Form() {
           id="name"
           type="text"
           value={name}
+          required
+          name="name"
           onChange={handleChange}
           className="flex-grow p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-600"
           placeholder="Enter name"
@@ -45,9 +67,10 @@ export default function Form() {
             <input
               id="expenseTypeIncome"
               onChange={handleChange}
-              value={type}
+              value="income"
               type="radio"
-              name="expenseType"
+              name="type"
+              checked={type === "income"}
               className="h-4 w-4 text-teal-600 border-gray-300 focus:ring-teal-500"
             />
             <label
@@ -61,9 +84,11 @@ export default function Form() {
             <input
               id="expenseTypeExpense"
               onChange={handleChange}
-              value={type}
+              value="expense"
               type="radio"
-              name="expenseType"
+              name="type"
+              required
+              checked={type === "expense"}
               className="h-4 w-4 text-teal-600 border-gray-300 focus:ring-teal-500"
             />
             <label
@@ -86,13 +111,24 @@ export default function Form() {
           onChange={handleChange}
           value={amount}
           type="number"
+          name="amount"
+          required
           className="flex-grow p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-600"
           placeholder="Enter amount"
         />
       </div>
-      <button className="w-full mt-4 bg-teal-600 text-white py-2 px-4 rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50">
+      <button
+        disabled={isLoading}
+        type="submit"
+        className="w-full mt-4 bg-teal-600 text-white py-2 px-4 rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50"
+      >
         Add Transaction
       </button>
+      {!isLoading && isError && (
+        <div className="text-center">
+          <p className="text-[18px] text-red-600 text-bold">There was an error occured</p>
+        </div>
+      )}
     </form>
   );
 }
