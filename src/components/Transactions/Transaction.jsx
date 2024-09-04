8decc2ba4a35b4d0bb96c591be966f077edc0886
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-04 11:29:47 +0600

    Transaction data fetched completed

--- src/components/Transactions/Transaction.jsx
+++ src/components/Transactions/Transaction.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/prop-types */
 import { FaBangladeshiTakaSign } from "react-icons/fa6";
-export default function Transaction() {
+export default function Transaction({transactionData}) {
+    
   return (
     <div className="flex justify-between p-3 bg-teal-600  rounded-md mt-[10px]">
-      <p className="text-white text-[16px] font-medium">Earned this month</p>
+      <p className="text-white text-[16px] font-medium">{transactionData?.name ?? ""}</p>
       <div className="flex items-center gap-[8px] ">
         <FaBangladeshiTakaSign size={16} color="white" />
-        <p className="text-[16px] font-medium text-white ml-[-4px]">100</p>
+        <p className="text-[16px] font-medium text-white ml-[-4px]">{transactionData?.amount ?? ""} </p>
         <img src="/edit.svg" height={16} width={16} alt="edit" className="cursor-pointer" />
         <img src="/delete.svg" height={16} width={16} alt="delete" className="cursor-pointer"/>
       </div>
--- src/components/Transactions/TransactionList.jsx
+++ src/components/Transactions/TransactionList.jsx
@@ -1,13 +1,47 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTransactions } from "../../features/transaction/transactionSlice";
 import Transaction from "./Transaction";
+
 export default function TransactionList() {
-  return (
-    <div className="text-center px-4">
-      <p className="text-[18px] font-medium">Your Transition</p>
-      <Transaction/>
-      <Transaction/>
-      <Transaction/>
-      <Transaction/>
-      <Transaction/>
-    </div>
+
+  const dispatch = useDispatch();
+  const { isLoading, isError, transactions } = useSelector(
+    (state) => state.transaction
   );
+
+  useEffect(() => {
+    dispatch(fetchTransactions());
+  }, [dispatch]);
+
+  // Corrected conditional rendering
+  let content = null;
+
+  if (isLoading) {
+    content = (
+      <p className="text-[20px] text-red-500 font-bold mt-[10px] text-center">Loading</p>
+    );
+  } else if (isError) {
+    content = (
+      <p className="text-[20px] text-red-500 font-bold mt-[10px] text-center">
+        Error Fetching Data
+      </p>
+    );
+  } else if (transactions.length === 0) {
+    content = (
+      <p className="text-[20px] text-red-500 font-bold mt-[10px] text-center">
+        Not Enough Data Available
+      </p>
+    );
+  } else {
+    content = (
+      <div>
+        {transactions.map((t) => (
+          <Transaction key={t.id} transactionData={t} />
+        ))}
+      </div>
+    );
+  }
+
+  return <div className="text-center px-4">{content}</div>;
 }
