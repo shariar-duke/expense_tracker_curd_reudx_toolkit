00df7ef9558db08fffd778e93184bf0b19d0110c
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-04 14:11:27 +0600

    action dispatched for Form edit data

--- src/components/Transactions/Transaction.jsx
+++ src/components/Transactions/Transaction.jsx
@@ -1,8 +1,15 @@
 /* eslint-disable react/prop-types */
 import { FaBangladeshiTakaSign } from "react-icons/fa6";
+import { useDispatch } from "react-redux";
+import { editActive } from "../../features/transaction/transactionSlice";
 export default function Transaction({transactionData}) {
-
+    const dispatch = useDispatch()
     const {name, amount , type } = transactionData
+
+    const handleEditSelect=()=> 
+    {
+       dispatch(editActive(transactionData))
+    }
     
   return (
     <div className={` ${type ==="income" ? "bg-teal-600" : "bg-red-400"} flex justify-between p-3  rounded-md mt-[10px]`}>
@@ -10,7 +17,7 @@ export default function Transaction({transactionData}) {
       <div className="flex items-center gap-[8px] ">
         <FaBangladeshiTakaSign size={16} color="white" />
         <p className="text-[16px] font-medium text-white ml-[-4px]">{amount} </p>
-        <img src="/edit.svg" height={16} width={16} alt="edit" className="cursor-pointer" />
+        <img onClick={handleEditSelect} src="/edit.svg" height={16} width={16} alt="edit" className="cursor-pointer" />
         <img src="/delete.svg" height={16} width={16} alt="delete" className="cursor-pointer"/>
       </div>
     </div>
